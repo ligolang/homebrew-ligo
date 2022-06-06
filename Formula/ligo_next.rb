bf60ac63d9ef31b3aadd9b40733c24e4f78b7980
stepan.prudnikov@serokell.io <prustephan98@gmail.com>
2022-06-06 21:09:11 +0500

    Fix autoupdate script

--- Formula/ligo_next.rb
+++ Formula/ligo_next.rb
@@ -2,9 +2,9 @@ class LigoNext < Formula
   desc "Friendly Smart Contract Language for Tezos"
   homepage "https://ligolang.org/"
 
-  version "c6e508fa6bb5e76ed64928d8311e51bf0175c12f"
+  version "2157dbd08f2598f850f95319f4ec33bf7cbd0c40"
   # We clone repo explicitely to preserve the information about git submodules
-  url "https://gitlab.com/ligolang/ligo.git", revision: "c6e508fa6bb5e76ed64928d8311e51bf0175c12f"
+  url "https://gitlab.com/ligolang/ligo.git", revision: "2157dbd08f2598f850f95319f4ec33bf7cbd0c40"
 
   build_dependencies = %w[opam rust hidapi pkg-config gnu-sed]
   build_dependencies.each do |dependency|
@@ -50,6 +50,6 @@ class LigoNext < Formula
   end
 
   test do
-    system "#{bin}/ligo", "--help=plain"
+    system "#{bin}/ligo", "--help"
   end
 end
--- scripts/update-formula.sh
+++ scripts/update-formula.sh
@@ -3,22 +3,22 @@
 # Check latest release and compare it to the current version
 latest_release_name="$(curl -X GET "https://gitlab.com/api/v4/projects/12294987/releases" | jq --raw-output '.[0].tag_name')"
 latest_release_revision="$(curl -X GET "https://gitlab.com/api/v4/projects/12294987/releases" | jq --raw-output '.[0].commit.id')"
-current_release_name="$(grep -Po 'tag: "\K.*?(?=")' Formula/ligo.rb)"
+current_release_name="$(ggrep -Po 'tag: "\K.*?(?=")' Formula/ligo.rb)"
 
 if [ "$current_release_name" = "$latest_release_name" ]; then
     echo "Release is up to date, doing nothing"
 else
     echo "New release detected, updating!"
 
-    sed -i "s|tag: \".*\", revision: \".*\"|tag: \"${latest_release_name}\", revision: \"${latest_release_revision}\"|g" Formula/ligo.rb
+    sed -i '' -e "s|tag: \".*\", revision: \".*\"|tag: \"${latest_release_name}\", revision: \"${latest_release_revision}\"|g" Formula/ligo.rb
 fi
 
 latest_next_commit_hash="$(curl -X GET "https://gitlab.com/api/v4/projects/12294987/repository/commits" | jq --raw-output '.[0].id')"
-current_next_commit_hash="$(grep -Po 'revision: "\K.*?(?=")' Formula/ligo_next.rb)"
+current_next_commit_hash="$(ggrep -Po 'revision: "\K.*?(?=")' Formula/ligo_next.rb)"
 
 if [ "$current_next_commit_hash" = "$latest_next_commit_hash" ]; then
     echo "Next is up to date, doing nothing"
 else
     echo "New commit detected, updating!"
-    sed -i "s|${current_next_commit_hash}|${latest_next_commit_hash}|g" Formula/ligo_next.rb
+    sed -i '' -e "s|${current_next_commit_hash}|${latest_next_commit_hash}|g" Formula/ligo_next.rb
 fi
